81131e15c83bd32371320fa50b69fa10813cfaf5
Alexia <alexia.skrzypczak@gmail.com>
2022-02-03 13:10:18 +0100

    add form to photographer page

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,33 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+async function displayData(media, photographers) {
+  const photographerSection = document.querySelector(".photograph-header")
+
+  const mediaSection = document.querySelector("#main")
+  console.log(mediaSection)
+
+  const selectForm = document.createElement("form")
+  selectForm.className = "selectForm"
+  mediaSection.appendChild(selectForm)
+
+  const selectLabel = document.createElement("label")
+  selectLabel.className = "selectLabel"
+  selectLabel.textContent = "Trier par"
+  selectForm.appendChild(selectLabel)
+
+  const selectOption = document.createElement("select")
+  selectOption.className = "selectLabel"
+  selectForm.appendChild(selectOption)
+
+  const popularityOption = document.createElement("option")
+  popularityOption.className = "popularityOption"
+  popularityOption.setAttribute("value", "popularité")
+  popularityOption.textContent = "Popularité"
+  selectOption.appendChild(popularityOption)
+
+  media.forEach((picture) => {
+    const mediaModel = mediaFactory(picture)
+    const pictureDOM = mediaModel.getpictureCardDOM()
+    mediaSection.appendChild(pictureDOM)
+  })
+}
