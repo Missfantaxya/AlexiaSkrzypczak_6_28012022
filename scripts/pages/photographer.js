71ee7e76b9f5fea036a7bdec294bf5641e746440
Alexia <alexia.skrzypczak@gmail.com>
2022-02-18 12:01:45 +0100

    total des likes du photographe

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -10,21 +10,65 @@ let idUrl = parseInt(urlRechercheParams.get("id"))
 async function displayData(photographers, media) {
   // La parties avec le photographe :----------------------------------------
 
+  const heartSvg = "assets/icons/heart-solid.svg"
+
   const photographHeader = document.querySelector(".photograph-header")
   const contact = document.querySelector(".contact_button")
 
+  // tableau du photographe
   const photographer = photographers.filter(
     (photographer) => photographer.id == idUrl
   )
-  // console.log(photographer) //*ok
 
+  // Utilisation de la factory photographer pour afficher les détails du photographe
   const photographerModel = photographerFactory(photographer)
   const UserProfilDOM = photographerModel.getUserProfilDOM()
   photographHeader.insertBefore(UserProfilDOM, contact)
 
+  // Utilisation de la factory photographer pour afficher l'avatar du photographe
   const photographerAvatarModel = photographerFactory(photographer)
   const UserAvatarDOM = photographerAvatarModel.getUserAvatarDOM()
   photographHeader.appendChild(UserAvatarDOM)
+
+  // tableau avec les médias du photographe
+  const photographerMedias = media.filter(
+    (photographerMedia) => photographerMedia.photographerId == idUrl
+  )
+
+  // récupération des likes de chaque média du photographe
+  const mediasLikes = photographerMedias.map(
+    (oneMediaLikes) => oneMediaLikes.likes
+  )
+
+  // addition de tous les likes du photographe
+  let initialLike = 0
+  const photographerAllLikes = mediasLikes.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    initialLike
+  )
+
+  const priceAndLike = document.createElement("div")
+  priceAndLike.className = "priceAndLike"
+  photographHeader.appendChild(priceAndLike)
+
+  const wrapperLikes = document.createElement("div")
+  wrapperLikes.className = "wrapperLikes"
+  priceAndLike.appendChild(wrapperLikes)
+
+  const photographerLike = document.createElement("p")
+  photographerLike.className = "photographerLike"
+  photographerLike.textContent = photographerAllLikes
+  wrapperLikes.appendChild(photographerLike)
+
+  const heartphotographer = document.createElement("img")
+  heartphotographer.className = "heart"
+  heartphotographer.setAttribute("src", heartSvg)
+  wrapperLikes.appendChild(heartphotographer)
+
+  const photographerPrice = document.createElement("p")
+  photographerPrice.className = "photographerPrice"
+  photographerPrice.textContent = photographer[0].price + "€/jour"
+  priceAndLike.appendChild(photographerPrice)
 
   // La partie avec les médias :--------------------------------------
 
