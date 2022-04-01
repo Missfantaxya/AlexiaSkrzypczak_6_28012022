4b9700e480954d258c28259bc10a27b9c1b2a4a0
Alexia <alexia.skrzypczak@gmail.com>
2022-04-01 16:39:05 +0200

    post mentorat

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -16,29 +16,29 @@ function mediaFactory(data) {
   const videoMedia = `assets/photographies/${photographerId}/${video}`
 
   // TODO afficher le coeur différement pour pouvoir changer la couleur
-  const heartSvg = "assets/icons/heart-solid.svg"
+  const heartSvg = 'assets/icons/heart-solid.svg'
 
   function getpictureCardDOM() {
-    const article = document.createElement("a")
-    article.className = "photographie"
+    const article = document.createElement('a')
+    article.className = 'photographie'
 
-    if (data.hasOwnProperty("image")) {
-      article.setAttribute("href", picture)
-      const img = document.createElement("img")
-      img.className = "media"
-      img.setAttribute("src", picture)
-      img.setAttribute("alt", title)
-      img.setAttribute("aria-label", description)
+    if (data.hasOwnProperty('image')) {
+      article.setAttribute('href', picture)
+      const img = document.createElement('img')
+      img.className = 'media'
+      img.setAttribute('src', picture)
+      img.setAttribute('alt', title)
+      img.setAttribute('aria-label', description)
       article.appendChild(img)
-    } else if (data.hasOwnProperty("video")) {
-      article.setAttribute("href", videoMedia)
-      const thumbnail = document.createElement("video")
-      thumbnail.className = "media"
-      thumbnail.setAttribute("src", videoMedia)
-      thumbnail.setAttribute("type", "video/mp4")
-      thumbnail.setAttribute("alt", title)
+    } else if (data.hasOwnProperty('video')) {
+      article.setAttribute('href', videoMedia)
+      const thumbnail = document.createElement('video')
+      thumbnail.className = 'media'
+      thumbnail.setAttribute('src', videoMedia)
+      thumbnail.setAttribute('type', 'video/mp4')
+      thumbnail.setAttribute('alt', title)
 
-      thumbnail.setAttribute("aria-label", description)
+      thumbnail.setAttribute('aria-label', description)
       article.appendChild(thumbnail)
     }
 
@@ -61,140 +61,47 @@ function mediaFactory(data) {
     // })
 
     // cf Graphikart Lightbox pause à 34:24
-    /**
-     * @property {HTMLElement} element
-     */
-    // TODO ATTENTION la lightbox se créer plusieur fois
-    class Lightbox {
-      static init() {
-        const media = main.querySelectorAll(".photographie")
-        console.log("media", media) //* ok
-        media.forEach((media) =>
-          media.addEventListener("click", (e) => {
-            e.preventDefault()
-            // console.log(e.currentTarget.getAttribute("href")) //*ok
-            new Lightbox(e.currentTarget.getAttribute("href"))
-          })
-        )
-      }
 
-      /**
-       * @param {string} url URL de l'image
-       */
-      constructor(url) {
-        const body = document.querySelector("body")
-        // console.log("body : ", body) //* ok
-        const element = this.buildDom(url)
-        // console.log("element :", element) //*ok
-        body.appendChild(element)
-      }
-
-      // TODO ok mais n'en ferme qu'une à la fois et il y a plusieurs lightbox
-      /**
-       * Ferme le lightbox
-       * @param {MouseEvent} e
-       */
-      close(e) {
-        event.preventDefault()
-        // console.log("close => this : ", this) //* lightbox objet
-        // console.log("close => element : ", element) //! error is not defined
-        // console.log("close => this.element : ", this.element) //! undefioned
-        const element = document.querySelector(".lightbox")
-        // console.log("close => element : ", element) //* ok
-        element.classList.add("fadeOut")
-        window.setTimeout(() => {
-          element.parentElement.removeChild(element)
-        }, 500)
-      }
-
-      /**
-       * @param {string} url URL de l'image
-       * @return {HTMLelement}
-       */
-      buildDom(url) {
-        const dom = document.createElement("div")
-        dom.className = "lightbox"
-        dom.innerHTML = `<div class="lightbox__wrapper">
-        <button class="lightbox__close">
-          Fermer
-          <img
-            class="lightbox__cross"
-            src="assets/images/cross.svg"
-            alt="chevron vers la droite"
-          />
-        </button>
-        <button class="lightbox__prev">
-          précédent
-          <img
-            class="lightbox__arrow lightbox__arrow--prev"
-            src="assets/images/arrow.svg"
-            alt="chevron vers la gauche"
-          />
-        </button>
-        <div class="lightbox__container">
-          <img
-            src="${url}"
-            // TODO mettre un alt
-            alt=""
-          />
-           // TODO mettre le titre
-          <p class="lightbox__title">Arc-en-ciel</p>
-        </div>
-        <button class="lightbox__next">
-          suivant
-          <img
-            class="lightbox__arrow lightbox__arrow--next"
-            src="assets/images/arrow.svg"
-            alt="chevron vers la droite"
-          />
-        </button>`
-        dom
-          .querySelector(".lightbox__close")
-          .addEventListener("click", this.close.bind(this))
-        return dom
-      }
-    }
-
-    Lightbox.init()
-
-    const pictureDetails = document.createElement("div")
-    pictureDetails.className = "pictureDetails"
+    const pictureDetails = document.createElement('div')
+    pictureDetails.className = 'pictureDetails'
     article.appendChild(pictureDetails)
 
-    const pictureName = document.createElement("h2")
-    pictureName.className = "pictureName"
+    const pictureName = document.createElement('h2')
+    pictureName.className = 'pictureName'
     pictureName.textContent = title
     pictureDetails.appendChild(pictureName)
 
-    const contenairLikes = document.createElement("div")
-    contenairLikes.className = "contenairLikes"
+    const contenairLikes = document.createElement('div')
+    contenairLikes.className = 'contenairLikes'
     pictureDetails.appendChild(contenairLikes)
 
-    const pictureLikes = document.createElement("p")
-    pictureLikes.className = "pictureLikes"
+    const pictureLikes = document.createElement('p')
+    pictureLikes.className = 'pictureLikes'
     pictureLikes.textContent = likes
     contenairLikes.appendChild(pictureLikes)
 
     //incrémentations au click des likes des medias
-    contenairLikes.addEventListener("click", function () {
+    contenairLikes.addEventListener('click', function () {
       pictureLikes.textContent = ++likes
-      const allLikes = document.querySelector(".photographerLike")
+      const allLikes = document.querySelector('.photographerLike')
       // console.log("allLikes :", allLikes) //*ok
       // console.log("allLikes.innerHTML : ", allLikes.innerHTML) //*ok
       var allLikesValueNumber = parseInt(allLikes.innerHTML, 10)
       ++allLikesValueNumber
       // console.log("allLikesValueNumber :", allLikesValueNumber) //*ok
-      allLikes.innerHTML = ""
+      allLikes.innerHTML = ''
       allLikes.innerHTML = allLikesValueNumber
     })
 
-    const heartmedia = document.createElement("img")
-    heartmedia.className = "heart"
-    heartmedia.setAttribute("src", heartSvg)
+    const heartmedia = document.createElement('img')
+    heartmedia.className = 'heart'
+    heartmedia.setAttribute('src', heartSvg)
     contenairLikes.appendChild(heartmedia)
 
     return article
   }
+
+  // TODO ATTENTION la lightbox se créer plusieur fois
 
   // function getMediaCarousel() {
   //   const oneMediaCarousel = document.createElement("li")
@@ -225,6 +132,114 @@ function mediaFactory(data) {
 
   //   return oneMediaCarousel
   // }
+
+  /**
+   * @property {HTMLElement} element
+   */
+  class Lightbox {
+    static init() {
+      // document.querySelectorAll(".photographie").forEach((el) => {
+      //   console.log(el)
+      // })
+      // window.alert("alerte") //*ok
+      console.log(document) //*ok (structure html du document)
+      const test = document.querySelector('#main')
+      console.log(test) //* ok
+      console.log(document.querySelectorAll('.photographie')) //! vide Nodelist
+      console.log(document.querySelectorAll('div'))
+      const media = document.querySelectorAll('.photographie')
+      console.log('media', media) //! vide Nodelist
+      media.forEach((media) =>
+        media.addEventListener('click', (e) => {
+          e.preventDefault()
+          window.alert('alerte')
+          // console.log(e.currentTarget.getAttribute("href")) //*ok
+          new Lightbox(e.currentTarget.getAttribute('href'))
+        })
+      )
+    }
+    /**
+     * @param {string} url URL de l'image
+     */
+    constructor(url) {
+      const body = document.querySelector('body')
+      // console.log("body : ", body) //* ok
+      const element = this.buildDom(url)
+      // console.log("element :", element) //*ok
+      body.appendChild(element)
+    }
+
+    // TODO ok mais n'en ferme qu'une à la fois et il y a plusieurs lightbox
+    /**
+     * Ferme le lightbox
+     * @param {MouseEvent} e
+     */
+    close(e) {
+      event.preventDefault()
+      // console.log("close => this : ", this) //* lightbox objet
+      // console.log("close => element : ", element) //! error is not defined
+      // console.log("close => this.element : ", this.element) //! undefioned
+      const element = document.querySelector('.lightbox')
+      // console.log("close => element : ", element) //* ok
+      element.classList.add('fadeOut')
+      window.setTimeout(() => {
+        element.parentElement.removeChild(element)
+      }, 500)
+    }
+
+    /**
+     * @param {string} url URL de l'image
+     * @return {HTMLelement}
+     */
+    buildDom(url) {
+      const dom = document.createElement('div')
+      dom.className = 'lightbox'
+      dom.innerHTML = `<div class="lightbox__wrapper">
+        <button class="lightbox__close">
+          Fermer
+          <img
+            class="lightbox__cross"
+            src="assets/images/cross.svg"
+            alt="chevron vers la droite"
+          />
+        </button>
+        <button class="lightbox__prev">
+          précédent
+          <img
+            class="lightbox__arrow lightbox__arrow--prev"
+            src="assets/images/arrow.svg"
+            alt="chevron vers la gauche"
+          />
+        </button>
+        <div class="lightbox__container">
+        // TODO prévoir pour la vidéo
+          <img
+            src="${url}"
+            // TODO mettre un alt
+            alt=""
+          />
+           // TODO mettre le titre
+          <p class="lightbox__title">Arc-en-ciel</p>
+        </div>
+        <button class="lightbox__next">
+          suivant
+          <img
+            class="lightbox__arrow lightbox__arrow--next"
+            src="assets/images/arrow.svg"
+            alt="chevron vers la droite"
+          />
+        </button>`
+      dom
+        .querySelector('.lightbox__close')
+        .addEventListener('click', this.close.bind(this))
+      return dom
+    }
+  }
+  // Lightbox.init()
+
+  document.addEventListener('DOMContentLoaded', function () {
+    Lightbox.init()
+  })
 
   return {
     id,
