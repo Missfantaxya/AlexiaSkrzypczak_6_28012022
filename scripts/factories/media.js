fe48b5006feedc03fb8636a0a0a0af42e9d0cbce
Missfantaxya <alexia.skrzypczak@gmail.com>
2022-04-13 16:00:34 +0200

    lightbox unique WIP

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -1,4 +1,5 @@
-function mediaFactory(data) {
+function mediaFactory ( data )
+{
   let {
     id,
     date,
@@ -11,82 +12,82 @@ function mediaFactory(data) {
     title,
   } = data
 
-  const picture = `assets/photographies/${photographerId}/${image}`
+  const picture = `assets/photographies/${ photographerId }/${ image }`
 
-  const videoMedia = `assets/photographies/${photographerId}/${video}`
+  const videoMedia = `assets/photographies/${ photographerId }/${ video }`
 
   // TODO afficher le coeur différement pour pouvoir changer la couleur voir comme les croos de la lightbox en passant par figma
   const heartSvg = 'assets/icons/heart-solid.svg'
 
-  function getpictureCardDOM() {
-    const article = document.createElement('a')
+  function getpictureCardDOM ()
+  {
+    const article = document.createElement( 'a' )
     article.className = 'photographie'
 
-    if (data.hasOwnProperty('image')) {
-      article.setAttribute('href', picture)
-      const img = document.createElement('img')
+    if ( data.hasOwnProperty( 'image' ) )
+    {
+      article.setAttribute( 'href', picture )
+      const img = document.createElement( 'img' )
       img.className = 'media'
-      img.setAttribute('src', picture)
-      img.setAttribute('alt', title)
-      img.setAttribute('aria-label', description)
-      article.appendChild(img)
-    } else if (data.hasOwnProperty('video')) {
-      article.setAttribute('href', videoMedia)
-      const thumbnail = document.createElement('video')
+      img.setAttribute( 'src', picture )
+      img.setAttribute( 'alt', title )
+      img.setAttribute( 'aria-label', description )
+      article.appendChild( img )
+    } else if ( data.hasOwnProperty( 'video' ) )
+    {
+      article.setAttribute( 'href', videoMedia )
+      const thumbnail = document.createElement( 'video' )
       thumbnail.className = 'media'
-      thumbnail.setAttribute('src', videoMedia)
-      thumbnail.setAttribute('type', 'video/mp4')
-      thumbnail.setAttribute('alt', title)
+      thumbnail.setAttribute( 'src', videoMedia )
+      thumbnail.setAttribute( 'type', 'video/mp4' )
+      thumbnail.setAttribute( 'alt', title )
 
-      thumbnail.setAttribute('aria-label', description)
-      article.appendChild(thumbnail)
+      thumbnail.setAttribute( 'aria-label', description )
+      article.appendChild( thumbnail )
     }
 
-    const pictureDetails = document.createElement('div')
+    const pictureDetails = document.createElement( 'div' )
     pictureDetails.className = 'pictureDetails'
-    article.appendChild(pictureDetails)
+    article.appendChild( pictureDetails )
 
-    const pictureName = document.createElement('h2')
+    const pictureName = document.createElement( 'h2' )
     pictureName.className = 'pictureName'
     pictureName.textContent = title
-    pictureDetails.appendChild(pictureName)
+    pictureDetails.appendChild( pictureName )
 
-    const contenairLikes = document.createElement('div')
+    const contenairLikes = document.createElement( 'div' )
     contenairLikes.className = 'contenairLikes'
-    pictureDetails.appendChild(contenairLikes)
+    pictureDetails.appendChild( contenairLikes )
 
-    const pictureLikes = document.createElement('p')
+    const pictureLikes = document.createElement( 'p' )
     pictureLikes.className = 'pictureLikes'
     pictureLikes.textContent = likes
-    contenairLikes.appendChild(pictureLikes)
+    contenairLikes.appendChild( pictureLikes )
 
     //incrémentations au click des likes des medias
-    contenairLikes.addEventListener('click', function () {
+    contenairLikes.addEventListener( 'click', function ()
+    {
       pictureLikes.textContent = ++likes
-      const allLikes = document.querySelector('.photographerLike')
+      const allLikes = document.querySelector( '.photographerLike' )
       // console.log("allLikes :", allLikes) //*ok
       // console.log("allLikes.innerHTML : ", allLikes.innerHTML) //*ok
-      var allLikesValueNumber = parseInt(allLikes.innerHTML, 10)
+      var allLikesValueNumber = parseInt( allLikes.innerHTML, 10 )
       ++allLikesValueNumber
       // console.log("allLikesValueNumber :", allLikesValueNumber) //*ok
       allLikes.innerHTML = ''
       allLikes.innerHTML = allLikesValueNumber
-    })
+    } )
 
-    const heartmedia = document.createElement('img')
+    const heartmedia = document.createElement( 'img' )
     heartmedia.className = 'heart'
-    heartmedia.setAttribute('src', heartSvg)
-    contenairLikes.appendChild(heartmedia)
-
-    article.addEventListener('click', function () {
-      alertDisplay()
-    })
+    heartmedia.setAttribute( 'src', heartSvg )
+    contenairLikes.appendChild( heartmedia )
 
     return article
   }
 
   // TODO attention plusieurs lightBox sont générées.
-  // TODO bug Tracy Galindo : triangle man
+  // TODO bug dernier photo
   // TODO bugs avec les vidéos au milieu du tableau gallery
   // TODO faire fonctionner sur les vidéos
 
@@ -95,17 +96,20 @@ function mediaFactory(data) {
    * @property {string[]} gallery Chemins des images de la lightbox
    * @property {string} url Image actuellement affichée
    */
-  class Lightbox {
-    static init() {
-      const links = Array.from(document.querySelectorAll('.photographie'))
-      console.log('links', links) //* ok dans media.js
-      const gallery = links.map((link) => link.getAttribute('href'))
-      console.log('gallery :', gallery) //* ok dans media.js
-      links.forEach((links) =>
-        links.addEventListener('click', (e) => {
+  class Lightbox
+  {
+    static init ()
+    {
+      const links = Array.from( document.querySelectorAll( '.photographie' ) )
+      // console.log('links', links) //* ok dans media.js
+      const gallery = links.map( ( link ) => link.getAttribute( 'href' ) )
+      // console.log('gallery :', gallery) //* ok dans media.js
+      links.forEach( ( links ) =>
+        links.addEventListener( 'click', ( e ) =>
+        {
           e.preventDefault()
-          new Lightbox(e.currentTarget.getAttribute('href'), gallery)
-        })
+          new Lightbox( e.currentTarget.getAttribute( 'href' ), gallery )
+        } )
       )
     }
 
@@ -113,31 +117,36 @@ function mediaFactory(data) {
      * @param {string} url URL de l'image
      * @param {string[]} gallery Chemins des images de la lightbox
      */
-    constructor(url, gallery) {
-      const body = document.querySelector('body')
-      this.element = this.buildDom(url)
+    constructor ( url, gallery )
+    {
+      const body = document.querySelector( 'body' )
+      // if ( document.getElementsByClassName( "lightbox" ).length == 0 )
+      // { this.element = this.buildDom( url ) } //! 1 lightbox mais pas tous les média dedans selon position du média cliqué
+      this.element = this.buildDom( url )
       this.gallery = gallery
-      this.loadImage(url)
-      this.onKeyUp = this.onKeyUp.bind(this)
-      body.appendChild(this.element)
-      document.addEventListener('keyup', this.onKeyUp)
+      this.loadImage( url )
+      this.onKeyUp = this.onKeyUp.bind( this )
+      body.appendChild( this.element )
+      document.addEventListener( 'keyup', this.onKeyUp )
     }
 
     /**
      * @param {string} url URL de l'image
      *
      */
-    loadImage(url) {
+    loadImage ( url )
+    {
       this.url = null
       const image = new Image() //? demander pour comprendre
-      const container = this.element.querySelector('.lightbox__container')
-      const loader = document.createElement('div')
+      const container = this.element.querySelector( '.lightbox__container' )
+      const loader = document.createElement( 'div' )
       loader.className = 'lightbox__loader'
       container.innerHTML = ''
-      container.appendChild(loader)
-      image.onload = () => {
-        container.removeChild(loader)
-        container.appendChild(image)
+      container.appendChild( loader )
+      image.onload = () =>
+      {
+        container.removeChild( loader )
+        container.appendChild( image )
         this.url = url
       }
 
@@ -147,13 +156,17 @@ function mediaFactory(data) {
     /**
      * @param {keyboardEvent} e
      */
-    onKeyUp(e) {
-      if (e.key === 'Escape') {
-        this.close(e)
-      } else if (e.key === 'ArrowLeft') {
-        this.prev(e)
-      } else if (e.key === 'ArrowRight') {
-        this.next(e)
+    onKeyUp ( e )
+    {
+      if ( e.key === 'Escape' )
+      {
+        this.close( e )
+      } else if ( e.key === 'ArrowLeft' )
+      {
+        this.prev( e )
+      } else if ( e.key === 'ArrowRight' )
+      {
+        this.next( e )
       }
     }
 
@@ -162,47 +175,54 @@ function mediaFactory(data) {
      * Ferme la lightbox
      * @param {MouseEvent/KeyboardEvent} e
      */
-    close(e) {
+    close ( e )
+    {
       event.preventDefault()
-      this.element.classList.add('fadeOut')
-      window.setTimeout(() => {
-        this.element.parentElement.removeChild(this.element)
-      }, 500) //* ok une à la fois
-      document.removeEventListener('keyup', this.onKeyUp) //* ok
+      this.element.classList.add( 'fadeOut' )
+      window.setTimeout( () =>
+      {
+        this.element.parentElement.removeChild( this.element )
+      }, 500 ) //* ok une à la fois
+      document.removeEventListener( 'keyup', this.onKeyUp ) //* ok
     }
 
     /**
      * Passe au média suivante
      * @param {MouseEvent/KeyboardEvent} e
      */
-    next(e) {
+    next ( e )
+    {
       e.preventDefault()
-      let i = this.gallery.findIndex((media) => media === this.url)
-      if (i === this.gallery.length - 1) {
+      let i = this.gallery.findIndex( ( media ) => media === this.url )
+      if ( i === this.gallery.length - 1 )
+      {
         i = -1
       }
-      this.loadImage(this.gallery[i + 1])
+      this.loadImage( this.gallery[ i + 1 ] )
     }
 
     /**
      * Passe au média précedent
      * @param {MouseEvent/KeyboardEvent} e
      */
-    prev(e) {
+    prev ( e )
+    {
       e.preventDefault()
-      let i = this.gallery.findIndex((media) => media === this.url)
-      if (i === 0) {
+      let i = this.gallery.findIndex( ( media ) => media === this.url )
+      if ( i === 0 )
+      {
         i = this.gallery.length
       }
-      this.loadImage(this.gallery[i - 1])
+      this.loadImage( this.gallery[ i - 1 ] )
     }
 
     /**
      * @param {string} url URL de l'image
      * @return {HTMLelement}
      */
-    buildDom(url) {
-      const dom = document.createElement('div')
+    buildDom ( url )
+    {
+      const dom = document.createElement( 'div' )
       dom.className = 'lightbox'
       dom.innerHTML = `<div class="lightbox__wrapper">
         <button class="lightbox__close">
@@ -224,7 +244,7 @@ function mediaFactory(data) {
         <div class="lightbox__container">
          // TODO prévoir pour la vidéo
           <img
-            src="${url}"
+            src="${ url }"
             // TODO mettre un alt
             alt=""
           />
@@ -240,19 +260,27 @@ function mediaFactory(data) {
           />
         </button>`
       dom
-        .querySelector('.lightbox__close')
-        .addEventListener('click', this.close.bind(this))
+        .querySelector( '.lightbox__close' )
+        .addEventListener( 'click', this.close.bind( this ) )
       dom
-        .querySelector('.lightbox__next')
-        .addEventListener('click', this.next.bind(this))
+        .querySelector( '.lightbox__next' )
+        .addEventListener( 'click', this.next.bind( this ) )
       dom
-        .querySelector('.lightbox__prev')
-        .addEventListener('click', this.prev.bind(this))
+        .querySelector( '.lightbox__prev' )
+        .addEventListener( 'click', this.prev.bind( this ) )
       return dom
     }
   }
-  // si .lightbox" n'existe pas alors initier la lightbox sinon rien faire
+
+  // pour éviter plusieur lightbox :
+  // if ( document.getElementsByClassName( "lightbox" ).length == 0 )
+  // {
+  //   Lightbox.init()
+  // } //! ne fonctionne pas
+
   Lightbox.init() //*ok si dans média.js
+  console.log( "lightbox ? :", document.getElementsByClassName( "lightbox" ) )
+  console.log( "lightbox length :", document.getElementsByClassName( "lightbox" ).length )
 
   return {
     id,
@@ -264,6 +292,5 @@ function mediaFactory(data) {
     price,
     title,
     getpictureCardDOM,
-    // getMediaCarousel,
   }
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -4,7 +4,6 @@
 
 // TODO faire fonctionner le bouton de contact
 
-
 // récupération de l'id du photographe dans l'url
 let urlRechercheParams = new URLSearchParams(window.location.search)
 let idUrl = parseInt(urlRechercheParams.get("id"))
