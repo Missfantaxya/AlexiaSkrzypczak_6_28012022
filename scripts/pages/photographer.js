51739b388d07ec20f6a2707eb3c47fb858954d6a
Missfantaxya <alexia.skrzypczak@gmail.com>
2022-05-18 16:18:09 +0200

    WIP SELECT

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -199,6 +199,13 @@ async function displayData ( photographers, media )
   mediaOptionPopularity.textContent = "Popularité"
   mediaSortSelection.appendChild( mediaOptionPopularity )
 
+  // // TODO le retirer quand select fixé 
+  // const mediaOptionPopularity2 = document.createElement( "option" )
+  // mediaOptionPopularity2.className = "media__selectOption media__selectOption--popularity"
+  // mediaOptionPopularity2.setAttribute( "value", "popularity" )
+  // mediaOptionPopularity2.textContent = "Popularité"
+  // mediaSortSelection.appendChild( mediaOptionPopularity2 )
+
   const mediaOptionDate = document.createElement( "option" )
   mediaOptionDate.className = "media__selectOption media__selectOption--date"
   mediaOptionDate.setAttribute( "value", "date" )
@@ -212,9 +219,10 @@ async function displayData ( photographers, media )
   mediaSortSelection.appendChild( mediaOptionTitle )
 
   // ----- style -----
-
+  //TODO voir pour ne pas avoir l'option sélectioné dans les options selectionnable et pour avoir "popularité" selectionnable quand au tri tri en cours.
   var x, i, j, l, ll, selElmnt, a, b, c
   x = document.getElementsByClassName( "media__sortContainer" )
+  console.log( "x : ", x ) //* ok =[hdiv.media__sortContainer] HTMLCollection
   l = x.length
   console.log( "l : ", l ) //* ok =1
   for ( i = 0; i < l; i++ )
@@ -222,47 +230,62 @@ async function displayData ( photographers, media )
     selElmnt = x[ i ].getElementsByTagName( "select" )[ 0 ]
     console.log( "selElmnt: ", selElmnt ) //*ok =le select élément html 
     ll = selElmnt.length
+    console.log( "ll : ", ll ) //*ok =3
     /* pour chaque élément, créez un nouveau DIV qui agira comme l'élément sélectionné : */
-    a = document.createElement( "DIV" )
-    a.setAttribute( "class", "select-selected" )
+    a = document.createElement( "div" )
+    a.className = "select-selected"
     a.innerHTML = selElmnt.options[ selElmnt.selectedIndex ].innerHTML
     x[ i ].appendChild( a )
+    console.log( "a : ", a ) //*ok =div "popularité"
     /* pour chaque élément, créez un nouveau DIV qui contiendra la liste d'options : */
-    b = document.createElement( "DIV" )
-    b.setAttribute( "class", "select-items select-hide" )
+    b = document.createElement( "div" )
+    b.className = "select-items select-hide"
     for ( j = 1; j < ll; j++ )
     {
       /* pour chaque option de l'élément de sélection d'origine, créez un nouveau DIV qui agira comme un élément d'option : */
-      c = document.createElement( "DIV" )
+      c = document.createElement( "div" )
+      console.log( "c : ", c )
       c.innerHTML = selElmnt.options[ j ].innerHTML
       c.addEventListener( "click", function ( e )
       {
         /* lorsqu'un élément est cliqué, mettre à jour la boîte de sélection d'origine, et l'élément sélectionné : */
         var y, i, k, s, h, sl, yl
         s = this.parentNode.parentNode.getElementsByTagName( "select" )[ 0 ]
+        console.log( "s : ", s ) //*ok
         sl = s.length
+        console.log( "sl : ", sl ) //*ok
         h = this.parentNode.previousSibling
+        console.log( "h : ", h ) //*ok
         for ( i = 0; i < sl; i++ )
         {
           if ( s.options[ i ].innerHTML == this.innerHTML )
           {
             s.selectedIndex = i
+            console.log( "s.selectedIndex  du if : ", s.selectedIndex ) //*ok
             h.innerHTML = this.innerHTML
+            console.log( "h.innerHTML du if : ", h.innerHTML ) //* ok
             y = this.parentNode.getElementsByClassName( "same-as-selected" )
+            console.log( "y du if : ", y ) //*ok
             yl = y.length
+            console.log( "yl du if : ", yl ) //*ok (o ou 1)
+            // retrait dela class "same-as-selected" de l'élément qui n'est plus sélectionné
             for ( k = 0; k < yl; k++ )
             {
               y[ k ].removeAttribute( "class" )
             }
-            this.setAttribute( "class", "same-as-selected" )
+            this.className = "same-as-selected"
             break
           }
         }
         h.click()
+        console.log( "h du clic : ", h )
       } )
       b.appendChild( c )
+      console.log( "b : ", b ) //* ok élément selctionné (div)
     }
-    x[ i ].appendChild( b )
+    x[ i ].appendChild( b ) //TODO voir si à refaire après chaque selection
+    console.log( "b après : ", b )
+    console.log( "a après : ", a )
     a.addEventListener( "click", function ( e )
     {
       /* lorsque la case de sélection est cliquée, fermez toutes les autres cases de sélection, et ouvrir/fermer la boîte de sélection actuelle : */
@@ -273,29 +296,45 @@ async function displayData ( photographers, media )
     } )
   }
 
+  /**
+   * Ferme toutes les case de selection du document sauf la boite actuelle
+   * @param {*} elmnt 
+   */
   function closeAllSelect ( elmnt )
   {
-    /* une fonction qui fermera toutes les cases de sélection du document, sauf la boîte de sélection actuelle : */
     var x, y, i, xl, yl, arrNo = []
     x = document.getElementsByClassName( "select-items" )
+    console.log( "x du close : ", x )
     y = document.getElementsByClassName( "select-selected" )
+    console.log( "y du close : ", y )
     xl = x.length
+    console.log( "xl du close : ", xl )
     yl = y.length
+    console.log( "yl du close : ", yl )
     for ( i = 0; i < yl; i++ )
     {
+      console.log( "i : ", i )
+      console.log( "y[ i ] du close (else) avant: ", y[ i ] )
+      console.log( "elmnt du if : ", elmnt )
       if ( elmnt == y[ i ] )
       {
+        console.log( "elmnt du close : ", elmnt )
         arrNo.push( i )
+        console.log( "arrNo : ", arrNo )
       } else
       {
+
         y[ i ].classList.remove( "select-arrow-active" )
+        console.log( "y[ i ] du close (else) après: ", y[ i ] )
       }
     }
     for ( i = 0; i < xl; i++ )
     {
       if ( arrNo.indexOf( i ) )
       {
+        console.log( "arrNo.indexOf( i ) : ", arrNo.indexOf( i ) )
         x[ i ].classList.add( "select-hide" )
+        console.log( "x[ i ] : ", x[ i ] )
       }
     }
   }
